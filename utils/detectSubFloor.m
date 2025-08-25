e37ed008c9dd8d1315e98657cbc6b773a6ccbad7
Kevinlzh9802 <zonghuan_li@outlook.com>
2025-08-25 22:36:02 +0200

    change settings

--- utils/detectSubFloor.m
+++ utils/detectSubFloor.m
@@ -15,7 +15,7 @@ outdir = 'output_directory';
 window_bounds = [1, 20] * 60;  % [min, max] window size
 step = 60;
 
-base_clue = "hip";
+base_clue = "foot";
 unique_segs = unique(data_results.Vid);
 formations = table;
 for u_ind=1:length(unique_segs)
@@ -98,64 +98,47 @@ for w = window_bounds(1):step:window_bounds(2)
 end
 
 %% Homogeneity and split score
-hm = zeros(4);
-sp = zeros(4);
-for k1=1:4
-    for k2=1:4
-        g1 = 0;
-        g2 = 0;
-        for i=1:height(data_results)
-            r1 = clues(k1) + "Res";
-            r2 = clues(k2) + "Res";
-            s1 = data_results.(r1){i};
-            s2 = data_results.(r2){i};
-            if ~isempty(s1) & ~isempty(s2)
-                data_results.homogeneity{i} = compute_homogeneity(s1, s2);
-                data_results.split_score{i} = compute_split_score(s1, s2);
-                g1 = g1 + 1;
-                g2 = g2 + 1;
-            else
-                data_results.homogeneity{i} = [];
-                data_results.split_score{i} = [];
-            end
-            
-        end
-        % r1,r2
-        hm(k1, k2) = sum(cell2mat(data_results.homogeneity)) / g1;
-        sp(k1, k2) = sum(cell2mat(data_results.split_score)) / g1;
-    end
-end
-heatmap(hm);
-figure;
-heatmap(sp);
-
-for sp_name = clues
-    if sp_name ~= base_clue
-        col_name = sp_name + "Split";
-        for k=1:height(formations)
-            formations.(col_name){k} = collectMatchingGroups(formations.participants{k}, ...
-                formations.longest_ts{k}, formations.Vid(k), ...
-                formations.Cam(k), data_results, sp_name, sp_merged, 61);
-            % a
-        end
-
-        % sp_dp = [];
-        % all_dp = [];
-        % for k1=1:height(formations)
-        %     a = formations.(col_name){k1};
-        %     [~, ls] = size(a);
-        %     for k2=k1:ls
-        %         sp_dp = [sp_dp, a{4,k2}];
-        %         all_dp = [all_dp, sum(a{4,k2})];
-        %     end
-        %     % a
-        % end
-        % sp_name
-        % [mean(sp_dp), std(sp_dp)]
-        % [mean(all_dp), std(all_dp)]
-    end
-end
-
+% hm = zeros(4);
+% sp = zeros(4);
+% for k1=1:4
+%     for k2=1:4
+%         g1 = 0;
+%         g2 = 0;
+%         for i=1:height(data_results)
+%             r1 = clues(k1) + "Res";
+%             r2 = clues(k2) + "Res";
+%             s1 = data_results.(r1){i};
+%             s2 = data_results.(r2){i};
+%             if ~isempty(s1) & ~isempty(s2)
+%                 data_results.homogeneity{i} = compute_homogeneity(s1, s2);
+%                 data_results.split_score{i} = compute_split_score(s1, s2);
+%                 g1 = g1 + 1;
+%                 g2 = g2 + 1;
+%             else
+%                 data_results.homogeneity{i} = [];
+%                 data_results.split_score{i} = [];
+%             end
+% 
+%         end
+%         % r1,r2
+%         hm(k1, k2) = sum(cell2mat(data_results.homogeneity)) / g1;
+%         sp(k1, k2) = sum(cell2mat(data_results.split_score)) / g1;
+%     end
+% end
+% heatmap(hm);
+% figure;
+% heatmap(sp);
+% 
+% for sp_name = clues
+%     if sp_name ~= base_clue
+%         col_name = sp_name + "Split";
+%         for k=1:height(formations)
+%             formations.(col_name){k} = collectMatchingGroups(formations.participants{k}, ...
+%                 formations.longest_ts{k}, formations.Vid(k), ...
+%                 formations.Cam(k), data_results, sp_name, sp_merged, 61);
+%         end
+%     end
+% end
 
 
 run plotFloorsCustom.m;
